c933d6c21c2dc29030b06d547dffcb4b70d861be
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-04-02 15:36:55 +0900

    Time: 8 ms (78.67%), Space: 42.3 MB (51.51%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,21 +2,19 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         Map<Character, Character> sMap = new HashMap<>();
         Map<Character, Character> tMap = new HashMap<>();
+        char[] Sarr = s.toCharArray();
+        char[] Tarr = t.toCharArray();
         for (int i = 0; i < s.length(); i++) {
-            if (sMap.containsKey(s.charAt(i))) {
-
-             if(sMap.get(s.charAt(i)) != t.charAt(i))
-                return false;
-            }
-            else
-                sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), t.charAt(i)));
-            if (tMap.containsKey(t.charAt(i))) {
-
-                if(tMap.get(t.charAt(i)) != s.charAt(i))
+            if (sMap.containsKey(Sarr[i])) {
+                if(sMap.get(Sarr[i]) != Tarr[i])
                     return false;
             }
-            else
-                tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), s.charAt(i)));
+            else {
+                if(tMap.containsKey(Tarr[i]))
+                    return false;
+                sMap.put(Sarr[i], Tarr[i]);
+                tMap.put(Tarr[i], Sarr[i]);
+            }
         }
         return true;
     }
