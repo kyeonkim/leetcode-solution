3c0bbebd3d9312f73272a2e48665e67c5972a319
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-04-10 14:14:53 +0900

    Time: 4 ms (86.41%), Space: 42.6 MB (99.76%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -5,23 +5,15 @@ class Solution {
         boolean skip = false;
         int indexInDeck = 0;
         int indexInResult = 0;
-
         Arrays.sort(deck);
-
         while (indexInDeck < N) {
-            // There is an available gap in result
             if (result[indexInResult] == 0) {
-
-                // Add a card to result
                 if (!skip) {
                     result[indexInResult] = deck[indexInDeck];
                     indexInDeck++;
                 }
-
-                // Toggle skip to alternate between adding and skipping cards
                 skip = !skip;
             }
-            // Progress to next index of result array
             indexInResult = (indexInResult + 1) % N;
         }
         return result;
