a7bc56c3e7aa7d0bb882a8ffade08516e209d8cf
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-04-12 09:00:38 +0900

    Time: 4 ms (34.72%), Space: 62.4 MB (68.16%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -2,7 +2,7 @@ class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         for (int i = 0; i < nums2.length; i++) {
             int idx = Arrays.binarySearch(nums1, nums2[i]);
-            if (idx > 0)
+            if (idx >= 0)
                 return nums2[i];
         }
         return -1;
