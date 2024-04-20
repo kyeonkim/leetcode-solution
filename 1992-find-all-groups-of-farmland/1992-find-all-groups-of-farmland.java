03fc9744adf7d2266fe9b89ee76a57f2764935c6
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-04-20 19:48:44 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[][] findFarmland(int[][] land) {
+        int farmCnt = 0;
+        
+        int r1 = 0, c1 = 0, r2 = 0, c2 = 0;
+        if (farmCnt == 0)
+            return new int[0][0];
+        int[][] answer = new int[farmCnt][4];
+        return new answer;
+    }
+}
