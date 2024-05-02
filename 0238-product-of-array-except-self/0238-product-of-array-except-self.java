e7990b3eef1e2b9b1f2a0cda4e3d5ff6b6d4f9d2
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-05-02 16:35:40 +0900

    Time: 2 ms (80.09%), Space: 56.2 MB (6.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -6,17 +6,13 @@ class Solution {
             prefix[i] = 1;
             suffix[i] = 1;
         }
-        prefix[0] = nums[0];
-        suffix[nums.length - 1] = nums[nums.length - 1];
-        for (int i = 1; i < nums.length; i++) {
-            prefix[i] = nums[i] * prefix[i - 1];
-            suffix[nums.length - 1 - i] = nums[nums.length - i - 1] * suffix[nums.length - i];
-        }
+        for (int i = 1; i < nums.length; i++)
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        for (int i = nums.length - 2; i >= 0; i--)
+            suffix[i] = suffix[i + 1] * nums[i + 1];
         int[] product = new int[nums.length];
-        product[0] = suffix[1];
-        product[nums.length - 1] = prefix[nums.length - 2];
-        for (int i = 1; i < nums.length - 1; i++)
-            product[i] = prefix[i - 1] * suffix[i + 1];
+        for (int i = 0; i < nums.length; i++)
+            product[i] = prefix[i] * suffix[i];
         return product;
     }
 }
