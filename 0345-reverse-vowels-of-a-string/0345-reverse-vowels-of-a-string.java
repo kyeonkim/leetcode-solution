e45122c8f777a2a72e4a634584d752e67be1ac07
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-05-02 14:11:41 +0900

    Time: 3 ms (79.65%), Space: 44.7 MB (73.79%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public String reverseVowels(String s) {
         char[] chars = s.toCharArray();
-        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        String vowels = "aeiouAEIOU";
         int start = 0, end = chars.length - 1;
         while (start < end) {
-            while (start < end && !vowels.contains(Character.toLowerCase(chars[start])))
+            while (start < end && vowels.indexOf(chars[start]) == -1)
                 start++;
-            while (start < end && !vowels.contains(Character.toLowerCase(chars[end])))
+            while (start < end && vowels.indexOf(chars[end]) == -1)
                 end--;
             char temp = chars[start];
             chars[start] = chars[end];
