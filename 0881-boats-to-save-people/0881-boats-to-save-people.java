fb457dd2988bfce070148c93a986dc8f9f0d1782
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-05-04 14:57:46 +0900

    Time: 16 ms (97.17%), Space: 54.1 MB (55.93%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -3,26 +3,12 @@ class Solution {
         int s = 0, e = people.length - 1;
         int total = 0;
         Arrays.sort(people);
-        people = reverse(people, people.length);
         while (s <= e) {
-            if (s == e && people[s] <= limit)
-                total++;
-            else {
-                if (people[s] + people[e] <= limit)
-                    e--;
-                total++;
-            }
-            s++;
+            if (people[s] + people[e] <= limit)
+                s++;
+            total++;
+            e--;
         }
         return total;
-    }
-
-    private int[] reverse(int[] arr, int size) {
-        for (int i = 0; i < size / 2; i++) {
-            int temp = arr[i];
-            arr[i] = arr[size - 1 - i];
-            arr[size - 1 - i] = temp;
-        }
-        return arr;
     }
 }
