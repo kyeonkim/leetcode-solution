4f52873cd87e69e82cb8e277cc93d84c2e200b49
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-05-12 21:06:51 +0900

    Time: 3 ms (62.99%), Space: 45.7 MB (18.42%) - LeetHub

--- 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
+++ 2373-largest-local-values-in-a-matrix/2373-largest-local-values-in-a-matrix.java
@@ -1,24 +1,23 @@
 class Solution {
     public int[][] largestLocal(int[][] grid) {
-        int[] mask = new int[9];
-        int[][] result = new int[grid.length - 2][grid.length - 2];
-        for (int i = 2; i < grid.length; i++) {
-            for (int j = 2; j < grid.length; j++) {
-                mask[0] = grid[i - 2][j - 2];
-                mask[1] = grid[i - 2][j - 1];
-                mask[2] = grid[i - 2][j];
-
-                mask[3] = grid[i - 1][j - 2];
-                mask[4] = grid[i - 1][j - 1];
-                mask[5] = grid[i - 1][j];
-
-                mask[6] = grid[i][j - 2];
-                mask[7] = grid[i][j - 1];
-                mask[8] = grid[i][j];
-                Arrays.sort(mask);
-                result[i - 2][j - 2] = mask[mask.length - 1];
+        int n = grid.length;
+        int[][] ans = new int[n - 2][n - 2];
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = 0; j < n - 2; j++) {
+                ans[i][j] = largestMatrix(grid, i, j);
             }
         }
-        return result;
+        return ans;
+    }
+
+    private int largestMatrix(int[][] grid, int i, int j) {
+        int max = 0;
+        for (int k = i; k <= i + 2; k++) {
+            for (int l = j; l <= j + 2; l++) {
+                if (max < grid[k][l])
+                    max = grid[k][l];
+            }
+        }
+        return max;
     }
 }
