a73f8e11629a4f3bc822236dfd01a189a38f8cd0
kyeonkim <108860425+kyeonkim@users.noreply.github.com>
2024-05-16 17:55:02 +0900

    Time: 9 ms (97.72%), Space: 45.3 MB (73.35%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -1,35 +1,21 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
-        Set<Character> setWord1 = wordToCharSet(word1);
-        Set<Character> setWord2 = wordToCharSet(word2);
-        if (setWord1.size() != setWord2.size())
-            return false;
-        for (char ch : setWord2) {
-            if (!setWord1.contains(ch))
-                return false;
+        int[] s1 = new int[26];
+        int[] s2 = new int[26];
+        for (int i = 0; i < word1.length(); i++)
+            s1[word1.charAt(i) - 'a']++;
+        for (int i = 0; i < word2.length(); i++)
+            s2[word2.charAt(i) - 'a']++;
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i] == 0 && s2[i] != 0) return false;
+            if (s1[i] != 0 && s2[i] == 0) return false;
         }
-        List<Integer> word1Fre = sortedWordFre(word1);
-        List<Integer> word2Fre = sortedWordFre(word2);
-        for (int i = 0; i < word1Fre.size(); i++) {
-            if (!word1Fre.get(i).equals(word2Fre.get(i)))
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        for (int i = 0; i < s1.length; i++) {
+            if (s1[i] != s2[i])
                 return false;
         }
         return true;
-    }
-
-    private Set<Character> wordToCharSet(String word) {
-        char[] charArray = word.toCharArray();
-        Set<Character> charSet = new HashSet<>();
-        for (char c : charArray) charSet.add(c);
-        return charSet;
-    }
-
-    private List<Integer> sortedWordFre(String word) {
-        Map<Character, Integer> wordFre = new HashMap<>();
-        for (int i = 0; i < word.length(); i++)
-            wordFre.put(word.charAt(i), wordFre.getOrDefault(word.charAt(i), 0) + 1);
-        List<Integer> sortWordFre = new ArrayList<>(wordFre.values());
-        Collections.sort(sortWordFre);
-        return sortWordFre;
     }
 }
